e63b5cefe7ff85058addd3cb3cf097348762f6d8
farouk <bensaidf232@gmail.com>
2024-12-15 14:15:32 +0100

    testimonials animation

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,4 +1,5 @@
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
 
 const testimonials = [
   {
@@ -27,8 +28,22 @@ export const TestimonialsSection = () => {
       <div className="container">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 md:gap-8 lg:gap-12">
           {testimonials.map((testimonial, testimonialIndex) => (
-            <blockquote
+            <motion.blockquote
               key={testimonialIndex}
+              initial={{
+                opacity: 0,
+                y: 24,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+              }}
+              viewport={{ once: true }}
+              transition={{
+                delay: testimonialIndex * 0.5,
+                ease: "easeInOut",
+                duration: 1,
+              }}
               className={twMerge(
                 testimonialIndex === 2 && "md:hidden lg:block"
               )}
@@ -56,7 +71,7 @@ export const TestimonialsSection = () => {
                   </div>
                 </div>
               </cite>
-            </blockquote>
+            </motion.blockquote>
           ))}
         </div>
       </div>
