c00bdc570d863ad5fdc584adeca6eb8f95bfc529
farouk <bensaidf232@gmail.com>
2024-12-15 14:08:05 +0100

    latestposts animation

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -4,11 +4,23 @@ import { getPostColorFromCatagory } from "../utils/postUtils";
 import { Tag } from "../components/Tag";
 import { CutCornerButton } from "../components/CutCornerButton";
 import { twMerge } from "tailwind-merge";
+import { useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { motion } from "framer-motion";
 
 export const LatestPosts = (props: {
   latestPosts: CollectionEntry<"blog">[];
 }) => {
   const { latestPosts } = props;
+
+  const targetRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start end", "start center"],
+  });
+
+  const marginTop = useTransform(scrollYProgress, [0, 1], [0, 64]);
 
   return (
     <section className="py-60">
@@ -45,7 +57,13 @@ export const LatestPosts = (props: {
               )
             )}
           </div>
-          <div className="md:flex flex-col gap-8 hidden mt-16">
+          <motion.div
+            className="md:flex flex-col gap-8 hidden mt-16"
+            ref={targetRef}
+            style={{
+              marginTop,
+            }}
+          >
             {latestPosts.map(
               ({ data: { title, description, category } }, postIndex) => (
                 <Cards
@@ -66,7 +84,7 @@ export const LatestPosts = (props: {
                 </Cards>
               )
             )}
-          </div>
+          </motion.div>
         </div>
 
         <div className="flex justify-center mt-48 md:mt-32">
